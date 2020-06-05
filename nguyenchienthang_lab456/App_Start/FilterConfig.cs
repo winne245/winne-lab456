60b0e26a54ab9136dcd25a3c5b5a1bd1d3144407
Administrator <Administrator@PC232>
2020-06-05 13:52:25 +0700

    Add project files.

---
+++ nguyenchienthang_lab456/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace nguyenchienthang_lab456
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ nguyenchienthang_lab456/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(nguyenchienthang_lab456.Startup))]
+namespace nguyenchienthang_lab456
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
