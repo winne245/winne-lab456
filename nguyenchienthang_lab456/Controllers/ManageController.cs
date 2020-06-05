10395726811386656fe11ceebaf73d00a3ca1544
Administrator <Administrator@PC232>
2020-06-05 13:54:34 +0700

    test connect github

--- nguyenchienthang_lab456/Controllers/ManageController.cs
+++ nguyenchienthang_lab456/Controllers/ManageController.cs
@@ -16,6 +16,7 @@ namespace nguyenchienthang_lab456.Controllers
         private ApplicationSignInManager _signInManager;
         private ApplicationUserManager _userManager;
 
+
         public ManageController()
         {
         }
